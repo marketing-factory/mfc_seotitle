e83f176efa6e441f1b83935d20a38d384fedd1c7
Alexander Schnitzler <aschnitzler@marketing-factory.de>
2015-11-06 09:51:44 +0000

    [TASK] Mark extension compatible with TYPO3 7 and PHP 5.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-5.3.99',
-			'typo3' => '4.5.0-6.2.99',
+			'php' => '5.3.0-5.5.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
